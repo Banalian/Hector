706bcdfe2b574aec8b071502480595308eb9dc9d
AdamWizard <48995051+AdamWizard@users.noreply.github.com>
2022-03-18 12:17:09 +0100

    Feat: create SousFamille.cs

---
+++ Hector/Model/SousFamille.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hector.Model
+{
+    class SousFamille
+    {
+        private int RefSousFamille;
+        private Famille refFamille;
+        private String nom;
+
+    }
+}
