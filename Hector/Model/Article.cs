5677a23c86e914470e6db969190b170635302932
AdamWizard <48995051+AdamWizard@users.noreply.github.com>
2022-03-18 12:13:41 +0100

    Feat: create Article.cs

---
+++ Hector/Model/Article.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hector.Model
+{
+    class Article
+    {
+        private int IdArticle;
+        private String Description;
+        private String Reference;
+        //private Marque Marque;
+        //private Famille Famille;
+        //private SousFamille SousFamille;
+
+        private float PrixHT;
+
+        public Article() {
+            Description = "";
+            Reference = "";
+            PrixHT = 0.0f;
+        }
+        
+
+
+    }
+}
