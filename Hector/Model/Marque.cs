bc273b1e14a9e50b9d8a3645998e71655685ab67
AdamWizard <48995051+AdamWizard@users.noreply.github.com>
2022-03-18 12:18:10 +0100

    Feat: create Marque.cs

---
+++ Hector/Model/Marque.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hector.Model
+{
+    class Marque
+    {
+        private int RefMarque;
+        private String nom;
+
+    }
+}
